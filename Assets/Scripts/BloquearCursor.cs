c355ac9408cc18c953e0044b18fbcdcb673ccafa
PalaciosIgn <72041740+PalaciosIgn@users.noreply.github.com>
2021-02-17 12:13:08 -0300

    Bloc Cursor

---
+++ Assets/Scripts/BloquearCursor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class BloquearCursor: MonoBehaviour
+{
+    void Update()
+    {
+        //Press the space bar to apply no locking to the Cursor
+        if (Input.GetKey(KeyCode.Escape))
+            Cursor.lockState = CursorLockMode.None;
+    }
+
+    void OnGUI()
+    {
+        //Press this button to lock the Cursor
+        if (GUI.Button(new Rect(5, 0, 100, 50), "Bloc Cursor"))
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
+}
